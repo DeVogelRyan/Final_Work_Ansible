c85723a270648f70b3c727cf8103522663c6cfc9
DeVogelRyan <devogelryan2@gmail.com>
2023-02-19 10:28:13 +0100

    added all vars for the vpn tunnel

--- vpn_tunnel/run.py
+++ vpn_tunnel/run.py
@@ -9,5 +9,8 @@ tunnelDestination = input("[R1] Tunnel destination: ")
 R2tunnelIP = input("[R2] Tunnel ip address: ")
 
 file = open('variables.yml', 'a') # Open a file in append mode
-file.write(f'\n{R1tunnelIP}') # Write some text
+file.write(f'\nR1tunnelIP: {R1tunnelIP}') # Write some text
+file.write(f'\ntunnelSource: {tunnelSource}') # Write some text
+file.write(f'\ntunnelDestination: {tunnelDestination}') # Write some text
+file.write(f'\nR2tunnelIP: {R2tunnelIP}') # Write some text
 file.close() # Close the file
