509ab24cc2d07abb1774e2da408235c4138c8805
DeVogelRyan <devogelryan2@gmail.com>
2023-02-20 14:59:54 +0100

    added input checks

--- vpn_tunnel/run.py
+++ vpn_tunnel/run.py
@@ -1,10 +1,45 @@
 import os
 from network import OSPF
 # os.system("ansible-playbook main.yml -u cisco -k")
+import re
 
-newClass = OSPF("R1", "192.168.1.0", "0.0.0.255", 0)
-newClass.func()
 
+def valid_ip_address(ip_address):
+    match = re.match(
+        r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", ip_address)
+    return bool(match)
+
+
+def checkInput(ip, wildcard, area):
+    if (valid_ip_address(ip) == False):
+        print("Ip address was wrongly formatted!")
+    elif (valid_ip_address(wildcard) == False):
+        print("Wildcard was wrongly formatted!")
+    elif (not area.isnumeric()):
+        print("Wildcard was wrongly formatted!")
+
+
+def getOSPFNetworks(OSPFList: list, TAG: str):
+    while True:
+        print("setup a network for OSPF...")
+        print("Leave any number blank to stop.")
+        ip = input(f"[{TAG}] IP address: ")
+        wildcard = input(f"[{TAG}] Wildcard mask: ")
+        area = input(f"[{TAG}] Area: ")
+        checkInput(ip, wildcard, area)
+        if ip == "" or wildcard == "" or area == "":
+            break
+        OSPFList.append(OSPF(TAG, ip, wildcard, area))
+
+
+R1Networks = []
+R2Networks = []
+# getOSPFNetworks(R1Networks, "R1")
+# getOSPFNetworks(R2Networks, "R2")
+
+
+
+"""
 file = open('variables.yml', 'a')  # Open a file in append mode
 file.write(f'\nOSPF:')  # Write some text
 file.write(f'\n  network{newClass.id}:')  # Write some text
